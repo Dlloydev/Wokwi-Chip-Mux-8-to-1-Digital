7e641752a90a989289b195332141bef76333eea4
Dlloydev <dlloydev@testcor.ca>
2023-02-08 14:39:21 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -19,6 +19,7 @@ typedef struct {
 } chip_state_t;
 
 static void chip_timer_event(void *user_data);
+static void chip_pin_change(void *user_data, pin_t pin, uint32_t value);
 
 void chip_init(void) {
   chip_state_t *chip = malloc(sizeof(chip_state_t));
@@ -33,24 +34,52 @@ void chip_init(void) {
   chip->pin_OUT = pin_init("OUT", OUTPUT);
   chip->Select_attr = attr_init("Select", 0);
 
+  const pin_watch_config_t config = {
+    .edge = BOTH,
+    .pin_change = chip_pin_change,
+    .user_data = chip,
+  };
+  pin_watch(chip->pin_D1, &config);
+  pin_watch(chip->pin_D2, &config);
+  pin_watch(chip->pin_D3, &config);
+  pin_watch(chip->pin_D4, &config);
+  pin_watch(chip->pin_D5, &config);
+  pin_watch(chip->pin_D6, &config);
+  pin_watch(chip->pin_D7, &config);
+  pin_watch(chip->pin_D8, &config);
+
   const timer_config_t timer_config = {
     .callback = chip_timer_event,
     .user_data = chip,
   };
   timer_t timer_id = timer_init(&timer_config);
-  timer_start(timer_id, 1000, true);
+  timer_start(timer_id, 10000, true);
 }
 
 static void chip_timer_event(void *user_data) {
   chip_state_t *chip = (chip_state_t*)user_data;
-  chip->Select = attr_read(chip->Select_attr);
-  if (chip->Select == 8) pin_write(chip->pin_OUT, pin_read(chip->pin_D8));
-  else if (chip->Select == 7) pin_write(chip->pin_OUT, pin_read(chip->pin_D7));
-  else if (chip->Select == 6) pin_write(chip->pin_OUT, pin_read(chip->pin_D6));
-  else if (chip->Select == 5) pin_write(chip->pin_OUT, pin_read(chip->pin_D5));
-  else if (chip->Select == 4) pin_write(chip->pin_OUT, pin_read(chip->pin_D4));
-  else if (chip->Select == 3) pin_write(chip->pin_OUT, pin_read(chip->pin_D3));
-  else if (chip->Select == 2) pin_write(chip->pin_OUT, pin_read(chip->pin_D2));
-  else if (chip->Select == 1) pin_write(chip->pin_OUT, pin_read(chip->pin_D1));
-  else pin_write(chip->pin_OUT, 0);
+  if (attr_read(chip->Select_attr) != chip->Select) {
+    chip->Select = attr_read(chip->Select_attr);
+    if (chip->Select == 8) pin_write(chip->pin_OUT, pin_read(chip->pin_D8));
+    else if (chip->Select == 7) pin_write(chip->pin_OUT, pin_read(chip->pin_D7));
+    else if (chip->Select == 6) pin_write(chip->pin_OUT, pin_read(chip->pin_D6));
+    else if (chip->Select == 5) pin_write(chip->pin_OUT, pin_read(chip->pin_D5));
+    else if (chip->Select == 4) pin_write(chip->pin_OUT, pin_read(chip->pin_D4));
+    else if (chip->Select == 3) pin_write(chip->pin_OUT, pin_read(chip->pin_D3));
+    else if (chip->Select == 2) pin_write(chip->pin_OUT, pin_read(chip->pin_D2));
+    else if (chip->Select == 1) pin_write(chip->pin_OUT, pin_read(chip->pin_D1));
+    else pin_write(chip->pin_OUT, 0);
+  }
+}
+
+static void chip_pin_change(void *user_data, pin_t pin, uint32_t value) {
+  chip_state_t *chip = (chip_state_t*)user_data;
+  if (chip->Select == 8 && pin == chip->pin_D8) pin_write(chip->pin_OUT, value);
+  else if (chip->Select == 7 && pin == chip->pin_D7) pin_write(chip->pin_OUT, value);
+  else if (chip->Select == 6 && pin == chip->pin_D6) pin_write(chip->pin_OUT, value);
+  else if (chip->Select == 5 && pin == chip->pin_D5) pin_write(chip->pin_OUT, value);
+  else if (chip->Select == 4 && pin == chip->pin_D4) pin_write(chip->pin_OUT, value);
+  else if (chip->Select == 3 && pin == chip->pin_D3) pin_write(chip->pin_OUT, value);
+  else if (chip->Select == 2 && pin == chip->pin_D2) pin_write(chip->pin_OUT, value);
+  else if (chip->Select == 1 && pin == chip->pin_D1) pin_write(chip->pin_OUT, value);
 }
